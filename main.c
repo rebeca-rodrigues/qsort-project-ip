7777108289eeafebf2961ea20e1ee3332902ed5a
Rebeca Rodrigues <beca.arq@gmail.com>
2024-06-03 20:38:36 -0300

    adaptado para testes de tempo de execução com arrays longas

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <time.h> 
 #include <stdlib.h>
-//#include <string.h>
-
-
 #include <unistd.h>
+
 #define Sleep(x) usleep((x)*1000000)
 const float DELAY = 1;
 
@@ -13,8 +11,8 @@ const float DELAY = 1;
 //Declaração de funções
 long getTime();
 int *geraArray (int, long);
-int *ordenaQSort (int *, int);
-int *ordenaISort (int *, int);
+int *ordenaQSort (int *, long int);
+int *ordenaISort (int *, long int);
 int cmpfunc (const void *, const void *);
 int *array;
 //void lerArquivo();
@@ -31,15 +29,20 @@ void main () {
 	printf("\nPrograma de comparação dos tempos de execução do qsort e insertion sort.\n\n");
 	
 	//Gera array de numeros aleatorios
-	int tamanhoArray = 0;
+	long int tamanhoArray = 0;
 	int * arrayDesordenada;
 	printf("Qual tamanho da sua array? ");
-	scanf(" %d", &tamanhoArray);
+	scanf(" %lu", &tamanhoArray);
 	arrayDesordenada = geraArray(tamanhoArray, semente);
-	printf("\nSua array de %d números desordenados é:\n\n", tamanhoArray);
+	/*
+	//Imprime array desordenada
+	printf("\nSua array de %ld números desordenados é:\n\n", tamanhoArray);
 	for (int i=0; i < tamanhoArray; i++){
 		printf("%d\n", arrayDesordenada[i]);
 	}
+	*/
+	printf("\n");
+	printf("Array desordenada:\n 1° número: %d\n Último número: %d\n", arrayDesordenada[0], arrayDesordenada[tamanhoArray-1]);
 	
 	/*QSort*/
 	
@@ -48,12 +51,16 @@ void main () {
 	int * arrayQSort = ordenaQSort(arrayDesordenada, tamanhoArray);
 	tempoFinal = getTime();
 	long tempoQSort = tempoFinal - tempoInicial;
-	
+	/*
 	//Imprime array ordenada com qsort
 	printf ("\nOrdenação por QSort:\n\n");
 	for (int i=0; i < tamanhoArray; i++){
 		printf("%d\n", arrayQSort[i]);
 	}
+	*/
+	printf("\n");
+	printf("Ordenação QSort:\n 1° número: %d\n Último número: %d\n", arrayQSort[0], arrayQSort[tamanhoArray-1]);
+	printf("Tempo de execução do Qsort (em nanosec): %ld\n\n", tempoQSort);
 	
 	
 	/*Insertion Sort*/
@@ -65,23 +72,20 @@ void main () {
 	tempoInicial = getTime();
 	int * arrayISort = ordenaISort(arrayDesordenada, tamanhoArray);
 	tempoFinal = getTime();
-	long tempoISort = tempoFinal - tempoInicial;
-	
+	long tempoISort = tempoFinal - tempoInicial;	
+	/*
 	//Imprime array ordenada com insertion sort
 	printf ("\nOrdenação por Insertion Sort:\n\n");
 	for (int i=0; i < tamanhoArray; i++){
 		printf("%d\n", arrayISort[i]);
 	}
-	
-	//Imprime tempos de execução
-	printf("\nTempo de execução do Qsort (em nanosec): %ld\n", tempoQSort);
-	printf("\nTempo de execução do Insertion sort (em nanosec): %ld\n", tempoISort);
+	*/
+	printf("Ordenação ISort:\n 1° número: %d\n Último número: %d\n", arrayISort[0], arrayISort[tamanhoArray-1]);
+	printf("Tempo de execução do Insertion sort (em nanosec): %ld\n\n", tempoISort);	
 	
 	//lerArquivo();
 	//escreverArquivo();
 	//Sleep(DELAY);
-
-  
 }
 
 long getTime (){
@@ -102,12 +106,16 @@ int *geraArray (int tamanho, long semente){
 	return array;
 }
 
-int *ordenaQSort (int * minhaArray, int tamanhoArray){
+int *ordenaQSort (int * minhaArray, long int tamanhoArray){
 	qsort (minhaArray, tamanhoArray, sizeof(int), cmpfunc);
 	return minhaArray;
 }
 
-int *ordenaISort (int * minhaArray, int tamanhoArray){
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int *ordenaISort (int * minhaArray, long int tamanhoArray){
 	int i;
 	for (int j=1; j<tamanhoArray; j++){
 		int chave = minhaArray[j];
@@ -118,15 +126,10 @@ int *ordenaISort (int * minhaArray, int tamanhoArray){
 		}
 		minhaArray[i+1]=chave;
 	}
-
 	return minhaArray;
-
 }
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
-}
-
+/*
 void lerArquivo() {
 	char textoArquivo[BUFLEN];
   	FILE *arquivo;
@@ -148,23 +151,5 @@ int escreverArquivo() {
 	FILE *fp;
 	//fp = fputs ();
 }
- 
-
-	/* No main
-	//Escolher operação, ler o gerar arquivo (ainda não implementado)
-	char operacao;
-	printf("- Para ler e ordenar array de um arquivo, digite 1. \n- Para gerar, ordenar e escrever um array aleatório em um arquivo, digite 2.\n");
-	scanf(" %c", &operacao);
-	
-	if (operacao == "1") {
-		printf("Operacao 1");
-	}
-	else if (operacao == "2"){
-		printf("Operacao 2");
-	}
-	else {
-		printf("Operacao invalida.");
-	}
-	*/
- 
+*/
  
