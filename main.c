1d8f563ef1e6e93adc29969aeeba9c6aac306548
Rebeca Rodrigues <beca.arq@gmail.com>
2024-06-06 16:42:07 -0300

    Otimização e limpeza do código

--- main.c
+++ main.c
@@ -3,13 +3,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int * array;
 long getTime();
-int *geraArray (int, long);
-void mostraArray (int *, long);
-int *ordenaQSort (int *, long int);
-int *ordenaISort (int *, long int);
+void geraArray (long, long), mostraArray (long), ordenaQSort (long), ordenaISort (long);
 int cmpfunc (const void *, const void *);
-int *array;
 
 void main () {
 	//Variáveis para contar tempo de execução
@@ -19,31 +16,30 @@ void main () {
 	printf("\nPrograma de comparação dos tempos de execução do qsort e insertion sort.\n\nQual tamanho da sua array? ");
 	scanf(" %lu", &tamanhoArray);
 	
-	//Gera array de numeros aleatorios com tamanho fornecido pelo usuário
-	array = geraArray(tamanhoArray, semente);
-	printf("\nArray desordenada: ");
-	mostraArray(array, tamanhoArray);
+	//Gera array de numeros aleatorios
+	geraArray(tamanhoArray, semente);
+	printf("\nArray desordenada: \n");
+	mostraArray(tamanhoArray);
 	
 	/*QSort*/
 	tempoInicial = getTime();
-	array = ordenaQSort(array, tamanhoArray);
+	ordenaQSort(tamanhoArray);
 	tempoFinal = getTime();
 	tempoQSort = tempoFinal - tempoInicial;
-	printf("\nOrdenação QSort: ");
-	mostraArray(array, tamanhoArray);
+	printf("\nOrdenação QSort: \n");
+	mostraArray(tamanhoArray);
 	printf("Tempo de execução do Qsort (em nanosec): %ld\n", tempoQSort);
-	
-	array = geraArray(tamanhoArray, semente); //Gera array novamente
+
+	geraArray(tamanhoArray, semente); //Gera array novamente
 	
 	/*Insertion Sort*/
 	tempoInicial = getTime();
-	array = ordenaISort(array, tamanhoArray);
+	ordenaISort(tamanhoArray);
 	tempoFinal = getTime();
 	tempoISort = tempoFinal - tempoInicial;
-	printf("\nOrdenação Insertion Sort: ");
-	mostraArray(array, tamanhoArray);
-	printf("Tempo de execução do Insertion sort (em nanosec): %ld\n\n", tempoISort);	
-	
+	printf("\nOrdenação Insertion Sort: \n");
+	mostraArray(tamanhoArray);
+	printf("Tempo de execução do Insertion sort (em nanosec): %ld\n", tempoISort);
 }
 
 long getTime (){
@@ -55,35 +51,30 @@ long getTime (){
 }
 
 
-int *geraArray (int tamanhoArray, long semente){
+void geraArray (long tamanhoArray, long semente){
 	array = malloc (tamanhoArray * sizeof(int));
 	srand (semente);
 	for (int i=0; i<tamanhoArray; i++){
 		array[i] = rand();
 	}
-	return array;
 }
 
-void mostraArray (int * array, long tamanhoArray){
-	/*
-	printf("\nSua array é:\n\n");
+void mostraArray (long tamanhoArray){
 	for (int i=0; i < tamanhoArray; i++){
-		printf("%d\n", arrayDesordenada[i]);
+		printf("%d\n", array[i]);
 	}
-	*/
-	printf("\n 1° número: %d\n Último número: %d\n", array[0], array[tamanhoArray-1]);
+	//printf("\n 1° número: %d\n Último número: %d\n", array[0], array[tamanhoArray-1]);
 }
 
-int *ordenaQSort (int * array, long int tamanhoArray){
+void ordenaQSort (long tamanhoArray){
 	qsort (array, tamanhoArray, sizeof(int), cmpfunc);
-	return array;
 }
 
 int cmpfunc (const void * a, const void * b) {
    return ( *(int*)a - *(int*)b );
 }
 
-int *ordenaISort (int * array, long int tamanhoArray){
+void ordenaISort (long tamanhoArray){
 	int i;
 	for (int j=1; j<tamanhoArray; j++){
 		int chave = array[j];
@@ -94,6 +85,4 @@ int *ordenaISort (int * array, long int tamanhoArray){
 		}
 		array[i+1]=chave;
 	}
-	return array;
 }
- 
